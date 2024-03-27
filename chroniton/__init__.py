d49dff6c6a7cab08af14cb8e71a8f61add248ebb
Ross Jennings <rossjjennings@gmail.com>
2024-03-27 12:07:11 -0400

    Add SplineModel

--- chroniton/__init__.py
+++ chroniton/__init__.py
@@ -1,5 +1,6 @@
 from .utils import fft_roll
 from .profile import Profile
+from .spline_model import SplineModel
 
 from . import _version
 __version__ = _version.get_versions()['version']
---
+++ chroniton/spline_model.py
@@ -0,0 +1,55 @@
+import numpy as np
+from scipy.interpolate import splev
+from scipy.signal import resample
+import pickle
+
+class SplineModel:
+    def __init__(self, mean_prof, eigvec, tck):
+        """
+        Create a new spline model of a pulse portrait.
+
+        Parameters
+        ----------
+        mean_prof: Mean profile shape. Should have shape (nbin,).
+        eigvec: Array of eigenvectors, of shape (nbin, neig).
+        tck: Tuple containing knot locations, B-spline coefficients,
+             and spline degree (as output by `scipy.interpolate.splprep()`).
+        """
+        self.mean_prof = mean_prof
+        self.eigvec = eigvec
+        self.tck = tck
+
+    @classmethod
+    def from_file(cls, filename):
+        """
+        Read a spline model from a pickle file like that output by
+        PulsePortraiture.
+
+        Parameters
+        ----------
+        filename: Path to pickle file from which to read the spline model.
+        """
+        with open(filename, "rb") as f:
+            model = pickle.load(f, encoding='latin1')
+        modelname, source, datafile, mean_prof, eigvec, tck = model
+        return cls(mean_prof, eigvec, tck)
+
+    def make_portrait(self, freqs, nbin=None):
+        """
+        Generate a portrait based on this spline model.
+        Equivalent to PulsePortraiture's `pplib.gen_spline_portrait()`.
+
+        Parameters
+        ----------
+        freqs: Frequencies (in MHz) at which to evaluate the model.
+        nbin: Number of phase bins to use in the model.
+              Data will be resampled if necessary.
+        """
+        proj_port = np.array(splev(freqs, self.tck, der=0, ext=0)).T
+        delta_port = np.dot(proj_port, self.eigvec.T)
+        port = delta_port + self.mean_prof
+        if nbin is not None and (nbin != self.mean_prof.shape[-1]):
+            shift = 0.5 * (nbin**-1 - len(mean_prof)**-1)
+            port = scipy.signal.resample(port, nbin, axis=1)
+            port = rotate_portrait(port, shift) #resample introduces shift!
+        return port
