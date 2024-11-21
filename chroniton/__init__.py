cc2d417f45a0fd4e2b10599cbc4b8321861cc69f
Ross Jennings <rossjjennings@gmail.com>
2024-11-21 16:49:39 -0500

    Import Portrait at top level

--- chroniton/__init__.py
+++ chroniton/__init__.py
@@ -1,5 +1,6 @@
 from .utils import fft_roll
 from .profile import Profile
+from .portrait import Portrait
 from .spline_model import SplineModel
 from .observation import Observation
 from .toas import toa_fourier
